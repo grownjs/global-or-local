4ea630bb39ca70e8c9a2b5be2780e24b3e28a499
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2019-03-30 22:27:19 -0600

    0.1.3

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function _resolveHack(name) {
 
     for (let i = 0; i < keys.length; i += 1) {
       if (name.indexOf(keys[i]) === 0) {
-        arguments[0] = _map[keys[i]] + name.substr(keys[i].length);
+        arguments[0] = path.join(_map[keys[i]], name.substr(keys[i].length));
         break;
       }
     }
@@ -147,7 +147,11 @@ const self = module.exports = {
     return self;
   },
   bind(prefix, baseDir) {
-    _map[prefix] = baseDir;
+    if (typeof prefix === 'object') {
+      Object.assign(_map, prefix);
+    } else {
+      _map[prefix] = baseDir;
+    }
 
     return self;
   },
